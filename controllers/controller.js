61da7445b17061115d163e889079e76b4bfa0f8e
Christopher Gladney <christophergladney282@gmail.com>
2023-11-21 13:26:48 +0000

    Ticket 3 for review

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,13 @@
-const { sendAllTopics } = require('../models/model');
+const { sendAllTopics, readEndPointsJSON } = require("../models/model");
 
 exports.getAllTopics = (req, res, next) => {
-    sendAllTopics().then(({ rows }) => {
-        res.status(200).send(rows)
-    })
-}
+  sendAllTopics().then(({ rows }) => {
+    res.status(200).send(rows);
+  });
+};
+
+exports.endpointsDescription = (req, res, next) => {
+  readEndPointsJSON().then((endpoints) => {
+    res.status(200).send(JSON.parse(endpoints));
+  });
+};
